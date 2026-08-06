cc4cac963b05fccdb821690f6e16fc0c2595cba6
laisa <laisacaz@hotmail.com>
2026-08-06 20:32:04 -0300

    add auth dependency injection

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.Analytics.Dashboard.Application.Auth.Interfaces;
+using Project.Analytics.Dashboard.Application.Auth.Services;
+
+namespace Project.Analytics.Dashboard.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+
+            return services;
+        }
+    }
+}
